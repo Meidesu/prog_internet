0db0335ff4ae3d4f48de377d0209507a864a0ba3
meidesu <antoniomeireles150@gmail.com>
2024-03-04 14:17:55 -0300

    first commit

--- Exercicio_01/q05.py
+++ Exercicio_01/q05.py
@@ -2,6 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 url = input('Digite a URL: ')
+text = input('Texto:')
 
 response = requests.get(url)
 
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+ocorrencias = soup.find_all_previous(name=True, string=text, limit=10)
+
+print(ocorrencias)
+
+for ocurr in ocorrencias:
+    print(ocurr.text)
